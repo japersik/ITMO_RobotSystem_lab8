a32f158cbec7fa6e2c929c1849c1404094131398
Vlad1slav <Vvlad1slavV@yandex.ru>
2020-05-26 21:30:01 +0300

    beta 0.11

--- Client/src/main/java/app/Styles.kt
+++ Client/src/main/java/app/Styles.kt
@@ -1,85 +1,44 @@
 package com.itmo.r3135.app
 
+import javafx.scene.paint.Color.color
 import javafx.scene.text.FontWeight
 import tornadofx.*
 import view.WorkView.ProductsSearch
 
 class Styles : Stylesheet() {
     companion object {
-        val heading by cssclass()
         val main by cssclass()
-        val neighborhood by cssclass()
-        val street by cssclass()
-        val schedule by cssclass()
-        val catPicture by cssclass()
-        val transparentOverlay by cssclass()
         val loginScreen by cssclass()
-        val zip by cssclass()
         val xy by cssclass()
         val productsSearch by cssclass()
+        val addform by cssclass()
+        val backgroundcolor = c("#a5ff9b")
+        val greencolor = c("#4e9830")
     }
 
     init {
-        s(form) {
+        addform{
             padding = box(50.px)
             prefWidth = 450.px
-
+            backgroundColor += backgroundcolor
             s(xy) {
                 maxWidth = 60.px
                 minWidth = maxWidth
             }
         }
-    }
-
-    init {
         loginScreen {
+            backgroundColor += backgroundcolor
             padding = box(15.px)
             vgap = 50.px
             hgap = 50.px
         }
-    }
-
-    init {
         productsSearch {
             padding = box(15.px)
             vgap = 10.px
             hgap = 10.px
         }
-    }
-
-    init {
-
         main {
-            backgroundColor += c("222222")
-        }
-
-        neighborhood {
-            backgroundColor += c("4E9830")
-            prefWidth = 470.px
-            prefHeight = 590.px
-        }
-
-        street {
-            backgroundColor += c("919191")
-        }
-
-        schedule {
-            prefWidth = 600.px
-        }
-
-        label and heading {
-            padding = box(10.px)
-            fontSize = 20.px
-            fontWeight = FontWeight.BOLD
-        }
-
-        catPicture {
-            prefWidth = 200.px
-            prefHeight = 100.px
-        }
-
-        transparentOverlay {
-            backgroundColor += c(0, 100, 100, 0.05)
+            backgroundColor += backgroundcolor
         }
     }
 }
--- Client/src/main/java/view/WorkView/MainView.kt
+++ Client/src/main/java/view/WorkView/MainView.kt
@@ -1,13 +1,15 @@
 package com.itmo.r3135.view
 
 
+import com.itmo.r3135.app.Styles
+import com.itmo.r3135.app.Styles.Companion.main
 import tornadofx.*
 import view.WorkView.*
 
 class MainView : View("BestApplication") {
 
     override val root = borderpane() {
-
+        addClass(main)
         prefHeight = 900.0
         prefWidth = 1600.0
         top(Toolbar::class)
